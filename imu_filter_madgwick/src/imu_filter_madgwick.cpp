9b5b9b4db6c92a8043c548583c20f6622f21baf3
Marcus M. Scheunemann <find@mms.ai>
2019-03-02 19:48:15 +0000

    subscribe to `imu/data_raw` and publish `imu/data`

--- imu_filter_madgwick/src/imu_filter_madgwick.cpp
+++ imu_filter_madgwick/src/imu_filter_madgwick.cpp
@@ -5,6 +5,14 @@ namespace imu_filter_madgwick {
 IMUFilterMadgwickPublisher::IMUFilterMadgwickPublisher()
   : rclcpp::Node{"imu_filter_madgwick_publisher"}
 {
+  imuPub_ = create_publisher<sensor_msgs::msg::Imu>("/imu/data");
+
+  imuRawSub_ = create_subscription<sensor_msgs::msg::Imu>(
+    "/imu/data_raw",
+    [ = ](sensor_msgs::msg::Imu::SharedPtr imuRawMsg) {
+      imuPub_->publish(imuRawMsg.get());
+    });
+
   // something
 }
 
