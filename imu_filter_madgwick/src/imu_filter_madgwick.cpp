f275deb7aa1626c248c0d5d8040d57690ff12826
Marcus M. Scheunemann <find@mms.ai>
2019-03-02 20:19:47 +0000

    update Madgwick and set quaternions

--- imu_filter_madgwick/src/imu_filter_madgwick.cpp
+++ imu_filter_madgwick/src/imu_filter_madgwick.cpp
@@ -1,4 +1,5 @@
 #include "imu_filter_madgwick/imu_filter_madgwick.hpp"
+#include "imu_filter_madgwick/MadgwickAHRS.h"
 
 namespace imu_filter_madgwick {
 
@@ -10,6 +11,21 @@ IMUFilterMadgwickPublisher::IMUFilterMadgwickPublisher()
   imuRawSub_ = create_subscription<sensor_msgs::msg::Imu>(
     "/imu/data_raw",
     [ = ](sensor_msgs::msg::Imu::SharedPtr imuRawMsg) {
+      double ax = imuRawMsg.get()->linear_acceleration.x;
+      double ay = imuRawMsg.get()->linear_acceleration.y;
+      double az = imuRawMsg.get()->linear_acceleration.z;
+
+      double gx = imuRawMsg.get()->angular_velocity.x;
+      double gy = imuRawMsg.get()->angular_velocity.y;
+      double gz = imuRawMsg.get()->angular_velocity.z;
+
+      MadgwickAHRSupdateIMU(gx, gy, gz, ax, ay, az);
+
+      imuRawMsg->orientation.w = q0;
+      imuRawMsg->orientation.x = q1;
+      imuRawMsg->orientation.y = q2;
+      imuRawMsg->orientation.z = q3;
+
       imuPub_->publish(imuRawMsg.get());
     });
 
