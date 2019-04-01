768c04f0b7e349b47db2974f4c1fd2deb7449f6e
Marcus M. Scheunemann <find@mms.ai>
2019-04-01 22:27:01 +0100

    create an arrow marker, fill with orientation from `imu/data` messages

--- rviz_marker/src/rviz_marker.cpp
+++ rviz_marker/src/rviz_marker.cpp
@@ -11,7 +11,26 @@ RvizMarkerPublisher::RvizMarkerPublisher()
   imuSub_ = create_subscription<sensor_msgs::msg::Imu>(
     "/imu/data",
     [ = ](sensor_msgs::msg::Imu::SharedPtr imuMsg) {
-      // create package
+
+      visualization_msgs::msg::Marker::SharedPtr markerMsg;
+      markerMsg->header.frame_id = "base_link";
+      // markerMsg->header.stamp = ros_clock.now();
+      markerMsg->ns = "imu_visualization";
+      markerMsg->id = 0;
+      markerMsg->type = visualization_msgs::msg::Marker::ARROW;
+      markerMsg->action = visualization_msgs::msg::Marker::ADD;
+
+      markerMsg->pose.position.x = 1;
+      markerMsg->pose.position.y = 1;
+      markerMsg->pose.position.z = 1;
+
+      markerMsg->pose.orientation.w = imuMsg.get()->orientation.w;
+      markerMsg->pose.orientation.x = imuMsg.get()->orientation.x;
+      markerMsg->pose.orientation.y = imuMsg.get()->orientation.y;
+      markerMsg->pose.orientation.z = imuMsg.get()->orientation.z;
+
+      markerPub_->publish(markerMsg.get());
+
     });
 
 }
