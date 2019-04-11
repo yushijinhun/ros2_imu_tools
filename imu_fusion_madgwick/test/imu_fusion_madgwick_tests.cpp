f6aa9f9727c7403e59a8cd8f48b1d49b7ef09bdc
Marcus M. Scheunemann <find@mms.ai>
2019-04-11 14:49:44 +0100

    more orientation tests

--- imu_fusion_madgwick/test/imu_fusion_madgwick_tests.cpp
+++ imu_fusion_madgwick/test/imu_fusion_madgwick_tests.cpp
@@ -20,6 +20,21 @@
 #include <gtest/gtest.h>
 #include <memory>
 
+template<typename T, int N>
+inline ::testing::AssertionResult VectorsEqual(
+  Eigen::Matrix<T, N, 1> const & expected,
+  Eigen::Matrix<T, N, 1> const & actual,
+  const double delta = 0.00001)
+{
+  double d = (expected - actual).norm();
+  if (d < delta) {
+    return ::testing::AssertionSuccess();
+  } else {
+    return ::testing::AssertionFailure() << "Actual: " << actual.transpose() << ", expected: " <<
+           expected.transpose() << " d = " << d;
+  }
+}
+
 inline ::testing::AssertionResult MatricesEqual(
   Eigen::MatrixXd const & expected,
   Eigen::MatrixXd const & actual,
@@ -36,8 +51,8 @@ inline ::testing::AssertionResult MatricesEqual(
 
 TEST(OrientationTests, construction)
 {
-  imu_fusion_madgwick::IMUFusionMadgwick imu;
-  Eigen::Quaterniond quaternion = imu.getEigenQuaternion();
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  auto quaternion = orientation.getEigenQuaternion();
 
   EXPECT_EQ(1.0, quaternion.w() );
   EXPECT_EQ(Eigen::Vector3d::Zero(), quaternion.vec() );
@@ -45,17 +60,171 @@ TEST(OrientationTests, construction)
 
 TEST(OrientationTests, gyro_halfPiXHighFreq)
 {
-  imu_fusion_madgwick::IMUFusionMadgwick imu_fusion;
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
 
   for (unsigned i = 0; i < 512; ++i) {
-    imu_fusion.integrate(Eigen::Vector3d{0.5 * M_PI, 0, 0}, 1.0 / 512);
+    orientation.integrate(Eigen::Vector3d{0.5 * M_PI, 0, 0}, 1.0 / 512);
   }
 
-  Eigen::Quaterniond quaternion = imu_fusion.getEigenQuaternion();
+  auto quaternion = orientation.getEigenQuaternion();
 
   EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(0.5 * M_PI, Eigen::Vector3d::UnitX()).matrix(),
     quaternion.matrix()));
 }
+
+TEST(OrientationTests, gyro_halfPiX)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+
+  orientation.integrate(Eigen::Vector3d{0.5 * M_PI, 0, 0}, 1.0);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(0.5 * M_PI, Eigen::Vector3d::UnitX()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_halfPiY)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+
+  orientation.integrate(Eigen::Vector3d{0, 0.5 * M_PI, 0}, 1.0);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(0.5 * M_PI, Eigen::Vector3d::UnitY()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_halfPiZ)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  orientation.integrate(Eigen::Vector3d{0, 0, 0.5 * M_PI}, 1.0);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(0.5 * M_PI, Eigen::Vector3d::UnitZ()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_piX)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  orientation.integrate(Eigen::Vector3d{0.5 * M_PI, 0, 0}, 2.0);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_piY)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  orientation.integrate(Eigen::Vector3d{0, 0.5 * M_PI, 0}, 2.0);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitY()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_piZ)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  orientation.integrate(Eigen::Vector3d{0, 0, 0.5 * M_PI}, 2.0);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitZ()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_quartPiX)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  orientation.integrate(Eigen::Vector3d{0.5 * M_PI, 0, 0}, .5);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(0.25 * M_PI, Eigen::Vector3d::UnitX()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_quartPiY)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  orientation.integrate(Eigen::Vector3d{0, 0.5 * M_PI, 0}, .5);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(0.25 * M_PI, Eigen::Vector3d::UnitY()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_quartPiZ)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  orientation.integrate(Eigen::Vector3d{0, 0, 0.5 * M_PI}, .5);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(0.25 * M_PI, Eigen::Vector3d::UnitZ()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_multiAxis)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  orientation.integrate(Eigen::Vector3d{1.0, 1.0, 1.0}.normalized() * 2.0 / 3.0 * M_PI, 1.0);
+
+  EXPECT_TRUE(MatricesEqual(Eigen::AngleAxisd(2.0 / 3.0 * M_PI,
+    Eigen::Vector3d(1.0, 1.0, 1.0).normalized()).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, gyro_zero)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  Eigen::Vector4d oldCoeffs = orientation.getEigenQuaternion().coeffs();
+  orientation.integrate(Eigen::Vector3d::Zero(), 0.008);
+
+  EXPECT_TRUE(MatricesEqual(oldCoeffs, orientation.getEigenQuaternion().coeffs()));
+}
+
+TEST(OrientationTests, gyro_chained)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  orientation.integrate(Eigen::Vector3d{0.5 * M_PI, 0, 0}, 1.0);
+  orientation.integrate(Eigen::Vector3d{0, 0, 0.5 * M_PI}, 1.0);
+
+  EXPECT_TRUE(MatricesEqual((Eigen::AngleAxisd(0.5 * M_PI, Eigen::Vector3d::UnitX()) *
+    Eigen::AngleAxisd(0.5 * M_PI, Eigen::Vector3d::UnitZ())).matrix(),
+    orientation.getEigenQuaternion().matrix()));
+}
+
+TEST(OrientationTests, merged_no_error)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  auto q0 = orientation.getEigenQuaternion();
+  orientation.integrate(Eigen::Vector3d::Zero(), 1.0, Eigen::Vector3d{0.0, 0.0, 1.0}, 0.1);
+
+  EXPECT_TRUE(VectorsEqual(q0.coeffs(), orientation.getEigenQuaternion().coeffs()));
+}
+
+TEST(OrientationTests, merged_turned_no_error)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+  // Turn 90 deg around x axis
+  orientation.integrate(Eigen::Vector3d{0.5 * M_PI, 0, 0}, 1.0);
+  auto q1 = orientation.getEigenQuaternion();
+  // Gravity is now along y axis
+  orientation.integrate(Eigen::Vector3d::Zero(), 1.0, Eigen::Vector3d{0.0, 1.0, 0.0}, 0.1);
+
+  EXPECT_TRUE(VectorsEqual(q1.coeffs(), orientation.getEigenQuaternion().coeffs()));
+}
+
+TEST(OrientationTests, merged_90deg_error)
+{
+  imu_fusion_madgwick::IMUFusionMadgwick orientation;
+
+  // Gravity is now along y axis
+  orientation.integrate(Eigen::Vector3d::Zero(), 1.0, Eigen::Vector3d{0.0, 1.0, 0.0}, 0.1);
+
+  EXPECT_FLOAT_EQ(1.0, orientation.getEigenQuaternion().norm());
+  EXPECT_LT(0.0, orientation.getEigenQuaternion().x());
+
+  // Test convergence after 20 seconds
+  for (unsigned i = 0; i < 10000; ++i) {
+    orientation.integrate(Eigen::Vector3d::Zero(), 1.0 / 500, Eigen::Vector3d{0.0, 1.0, 0.0}, 0.1);
+  }
+
+  // Quaternion element is sin(theta / 2)
+  EXPECT_NEAR(sin(.25 * M_PI), orientation.getEigenQuaternion().coeffs().x(), 0.00002);
+}
+
 
 int main(int argc, char ** argv)
 {
